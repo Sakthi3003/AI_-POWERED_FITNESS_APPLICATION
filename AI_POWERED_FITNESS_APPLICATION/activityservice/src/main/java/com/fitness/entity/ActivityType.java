62231e47e193049cad968729d8240aa334bb8fca
Sakthi3003 <tsakthisowmya30@gmail.com>
2025-07-06 22:47:31 +0530

    initial commit

---
+++ AI_POWERED_FITNESS_APPLICATION/activityservice/src/main/java/com/fitness/entity/ActivityType.java
@@ -0,0 +1,14 @@
+package com.fitness.entity;
+
+public enum ActivityType {
+    RUNNING,
+    WALKING,
+    CYCLING,
+    SWIMMING,
+    WEIGHT_TRAINING,
+    YOGA,
+    HIIT,
+    CARDIO,
+    STRETCH,
+    OTHERS
+}
